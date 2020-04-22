39d34a1c9a5b8a6cb164bb2764a480b696a1b0b7
Anwesh43 <anweshthecool0@gmail.com>
2020-04-23 03:19:28 +0530

    adding Canvas extension method

--- bouncyboxleftmoverview/src/main/java/com/anwesh/uiprojects/bouncyboxleftmoverview/BouncyBoxLeftMoverView.kt
+++ bouncyboxleftmoverview/src/main/java/com/anwesh/uiprojects/bouncyboxleftmoverview/BouncyBoxLeftMoverView.kt
@@ -14,7 +14,7 @@ import android.graphics.Color
 import android.graphics.RectF
 
 val nodes : Int = 5
-val lines : Int = 4
+val boxes : Int = 4
 val scGap : Float = 0.02f
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
@@ -26,3 +26,41 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBouncyBoxLeftMover(i : Int, scale : Float, gap : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    save()
+    translate(i * gap + gap * sf2, 0f)
+    drawRect(RectF(0f, 0f, size, size), paint)
+    drawLine(0f, 0f, gap * (sf1 - sf2), 0f, paint)
+    restore()
+}
+
+
+fun Canvas.drawBouncyBoxLeftMovers(scale : Float, w : Float, size : Float, paint : Paint) {
+    paint.style = Paint.Style.STROKE
+    val gap : Float = w / (boxes - 1)
+    for (j in 0..(boxes - 1)) {
+        drawBouncyBoxLeftMover(j, 0f, gap, size, paint)
+    }
+    paint.style = Paint.Style.FILL
+    val scDiv : Double = 1.0 / (boxes - 1)
+    val i : Int = Math.floor(scale.sinify() / scDiv).toInt()
+    drawBouncyBoxLeftMover(i, scale, gap, size, paint)
+}
+
+fun Canvas.drawBBLMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(0f, gap * (i + 1))
+    drawBouncyBoxLeftMovers(scale, w, size, paint)
+    restore()
+}
